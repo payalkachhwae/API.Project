af4f892c548e89404704d81001e367c8c3ea8f2e
Payal Kachhwae <kachhwaepayal@gmail.com>
2023-07-14 18:00:32 +0530

    first commit

---
+++ requirements.js
@@ -0,0 +1,51 @@
+//Requirement for our project
+
+//We are a book management company
+
+//BOOKS
+//ISBN, title, pub date, language, num page, author[], category[]
+
+//AUTHORS
+//id, name, books[]
+
+//PUBLICATIONS
+//id, name, books[]
+
+//We have to design and code an API over this .
+
+//1.BOOKS
+//We need an API :-
+//To get all the books
+//To get specific book
+//To get a list of books on category
+//To get a list of books based on languages
+
+//2.AUTHORS
+//We need an API :-
+//To get all the authors
+//To get a specific author based on id
+//To get a list of authors based on books
+
+//3.PUBLICATIONS
+//We need an API :-
+//To get all the publications
+//To get a specific publication
+//To get a list of publications based on a book
+
+
+
+//POST REQUEST
+//1. ADD NEW BOOK
+//2. ADD NEW PUBLICATION
+//3. ADD NEW AUTHOR
+
+
+
+/***********PUT************/
+//Update book details if author is changed.
+
+
+/***********DELETE***********/
+//1. Delete a book
+//2. Delete author from book
+//3. Delete author from book and related book from author

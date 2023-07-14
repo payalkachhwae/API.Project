cb71abea13057d8248c5777536db4d378504cf0c
Payal Kachhwae <kachhwaepayal@gmail.com>
2023-07-14 17:23:44 +0530

    first commit

---
+++ operators.js
@@ -0,0 +1,21 @@
+//mongodb Operators -> Powerfull
+
+//Logical Operators
+
+//$inc => increment, +1,+2,+3,-1,-2,-3
+//$min => minimum
+//$max => maximum
+//$set => Used to set a data
+//book.title = "xyz"
+
+//$unset => removing a property from an object
+
+//Array operators
+//$push
+//name = ["payal", "xyzzz"]
+//$pop
+//used to extract/remove/delete the last element
+//$pull: {
+//name:"xyzzz"
+//}
+//$addToSet => dosen't allow duplicates
